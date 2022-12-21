7d938ff84e09f9d96c4909d84da8218eccb62f81
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-12-21 20:44:09 -0300

    aprendiendo las herramientas pm2 y nodemon

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -15,3 +15,10 @@ console.log('Mi web es ' + web);
 
 
 console.log('imprimiendo otra cosas');
+
+// pm2 y nodemon son herramientas para ser mas felices
+// nodemon es una herramiente para el desarrollo, cada vez que detecte un
+// cambio en el programa o algun cambio en las dependencias de la misma,
+// se ejecuta automaticamente
+// pm2 es una herramienta para el monitoreo de una app, se utiliza
+// generalmente cuando la pp esta en produccion
