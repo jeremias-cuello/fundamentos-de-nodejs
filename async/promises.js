9303f461edf15a01ae4b9f839e57dff217d1df48
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-12-22 09:59:01 -0300

    otra de forma de manejar la asincronia - promesas

---
+++ async/promises.js
@@ -0,0 +1,50 @@
+/**
+ * las promesas son una clase global (asi como setInterval, etc.)
+ * nuestras funciones en lugar de ejecutar las cosas cuando sean
+ * devuelven una promesa, y quien las llame pueda tener la
+ * promesa y pueda estar pendiente de su estado, si fallo, si
+ * se realizo, etc.
+ */
+function hola(nombreHola){
+	return new Promise(function (resolve, reject){
+		setTimeout(() => {
+			console.log(`Hola, ${nombreHola}`);
+			resolve(nombreHola); // compartiendo parametro // adios(nombre)
+		}, 1500);
+	})
+}
+
+function hablar(nombre){
+	return new Promise((resolve, reject) => {
+		setTimeout(function(){
+			console.log('Bla Bla Bla Bla...');
+			resolve(nombre);
+		}, 1000)
+	})
+}
+
+function adios(nombre){
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log(`Adios ${nombre}`);
+			resolve();
+		}, 1000);
+	});
+}
+
+console.log('iniciando el proceso...');
+
+/* hola('Carlos')
+	.then(nombre => {
+		console.log('teminado el proceso');
+	}) */
+
+hola('Carlos')
+	.then(hablar)
+	.then(hablar)
+	.then(hablar)
+	.then(hablar)
+	.then(adios)
+	.then(() => {
+		console.log('teminado el proceso');
+	})
