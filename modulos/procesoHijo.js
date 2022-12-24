a6f71a9cfa366b79be26e58024ccafe2e7c27bf8
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-12-23 22:17:00 -0300

    desarrollando propiedades de los procesos | desarrollando eventos de los procesos

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,12 +1,32 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 
 // exec ejecuta comandos
-exec('node modulos/consola.js', (err, stdout, stderr) => { //  dir en windows y ls en linux o Mac
-	if (err) {
-		console.error(err);
-		return false;
-	}
+// exec('node modulos/consola.js', (err, stdout, stderr) => { //  dir en windows y ls en linux o Mac
+// 	if (err) {
+// 		console.error(err);
+// 		return false;
+// 	}
 
-	console.log(stdout);
-	console.log(stderr); // aca imprime los errores y los warning
-});
+// 	console.log(stdout);
+// 	console.log(stderr); // aca imprime los errores y los warning
+// });
+
+let proceso = spawn('tree', ['/a', '/f']);
+
+// console.log(proceso); // podemos ver las cosas que hay en el proceso
+console.log('¿cual es su PID?');
+console.log(proceso.pid);
+console.log('¿esta conectado?');
+console.log(proceso.connected);
+
+proceso.stdout.on('data', dato => {
+	console.log(dato.toString());
+})
+
+proceso.on('exit', () => {
+	console.log('el proceso termino');
+	console.log('¿esta muerto?');
+	console.log(proceso.killed);
+})
+
+/* para mas informacion sobre los eventos de los procesos hijos, siempre esta la documentacion de node */
