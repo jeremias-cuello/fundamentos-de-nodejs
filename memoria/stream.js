8e7ac73fb949736584b3aa65f21933067fc5567e
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-01-03 18:45:52 -0300

    trayendo los datos del archivo input.txt procesarlos y enviarlos al process.stdout

--- memoria/stream.js
+++ memoria/stream.js
@@ -3,9 +3,11 @@
  * de lectura: es un stream donde va soltando datos desde el origen y el destino los agarra.
  * de escritura: es donde el destino mete datos al origen.
  * bidireccional: escirtura y lectura simultanea.
- */
+*/
 
 const fs = require('fs');
+const stream = require('stream');
+const util = require('util');
 
 let data = '';
 const path = __dirname + '\\input.txt';
@@ -13,12 +15,40 @@ const path = __dirname + '\\input.txt';
 let redeableStream = fs.createReadStream(path);
 redeableStream.setEncoding('utf8');
 
+/*
 redeableStream.on('data', (chunk) => {
 	data += chunk;
 });
 redeableStream.on('end', (chunk) => {
 	console.log(data);
 });
+*/
 
 console.log('data:');
 console.log(data); // nada porque todavia no se setio, los eventos data y end son asincronos
+
+console.log('-------------------------------------');
+
+process.stdout.write('Mi');
+process.stdout.write('nombre');
+process.stdout.write('es');
+process.stdout.write('jere'); // process.stdout es un buffer del sistema. el metodo write escribe en él.
+console.log();
+
+const Transform = stream.Transform;
+
+function Mayus() {
+	Transform.call(this);
+}
+
+util.inherits(Mayus, Transform)
+
+Mayus.prototype._transform = function (chunk, codif, cb) {
+	chunkMayus = chunk.toString().toUpperCase();
+	this.push(chunkMayus);
+	cb();
+}
+
+let mayus = new Mayus();
+
+redeableStream.pipe(mayus).pipe(process.stdout);
