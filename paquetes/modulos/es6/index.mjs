cfafa0a85fb49d54d0cc9462a84f00d0e55bada9
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-01-02 21:28:15 -0300

    importando modulo nativo en es6

---
+++ paquetes/modulos/es6/index.mjs
@@ -0,0 +1,5 @@
+import modulo from "./modulo.mjs";
+
+modulo.saludar();
+console.log(modulo.prop1);
+console.log(modulo.prop2);
---
+++ paquetes/modulos/es6/modulo.mjs
@@ -0,0 +1,9 @@
+function saludar() {
+	console.log('Hola mundo!');
+}
+
+export default {
+	saludar,
+	prop1: 'Hola soy una propiedad 1 de un modulo experimental',
+	prop2: 'Hola soy una propiedad 2 de un modulo experimental'
+};
