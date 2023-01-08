e7ed432d9e3eddf47b98b8e06415e0cc333341b7
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-01-08 17:20:05 -0300

    usando archivos .js con import y from

--- paquetes/modulos/es6/archivoJS.js
+++ paquetes/modulos/es6/archivoJS.js
@@ -1,4 +1,13 @@
 
+/**
+ * ejecutar:
+ * npm init -y
+ *
+ * cuando seteamos a "módulo", el atributo "type" permite que un paquete (es6)
+ * especifique todos los archivos .js dentro de los módulos ES. Si no se especifica
+ * "tpe" o se setea a "commonjs", todos los archivos .js se tratan como CommonJS
+ */
+
 import modulo from "./modulo.mjs"; // SyntaxError: Cannot use import statement outside a module
 
 modulo.saludar();
