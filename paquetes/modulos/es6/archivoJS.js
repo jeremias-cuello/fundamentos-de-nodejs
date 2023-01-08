d4d53d91a677d6953518fe28048baea3a69a0a54
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-01-08 17:04:29 -0300

    usando sintaxis ES6 (import y from) en archivo .js
    agregando comentario de como usar sintaxis ES6 en archivos .js

---
+++ paquetes/modulos/es6/archivoJS.js
@@ -0,0 +1,6 @@
+
+import modulo from "./modulo.mjs"; // SyntaxError: Cannot use import statement outside a module
+
+modulo.saludar();
+console.log(modulo.prop1);
+console.log(modulo.prop2);
--- paquetes/modulos/es6/index.mjs
+++ paquetes/modulos/es6/index.mjs
@@ -1,3 +1,6 @@
+// cuando un archivo tiene la extension .msj NodeJS ya trata a este archivo con una sintaxis ES ("type": "module")
+// hay otra sintaxis que es la "type": "commonJS" que es con la que trata node con archivos .js
+// si queres usar la sintaxis ES con archivo .js debes agregar el atributo "type": "module" en tu package.json
 import modulo from "./modulo.mjs";
 
 modulo.saludar();
